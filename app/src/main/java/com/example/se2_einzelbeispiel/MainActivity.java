33ab73b812a466e1e6d5ed78e65f1b733ec2bad0
SabrinaMuh <sabrina.muhrer@aon.at>
2022-03-06 12:18:57 +0100

    start project and create thread

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -4,6 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+class MatrikelnrThread extends Thread{
+    String mnr;
+    String answer;
+
+    public MatrikelnrThread(String mnr){
+        this.mnr = mnr;
+    }
+
+    @Override
+    public void run(){
+        try {
+            Socket socket = new Socket("se2-isys.aau.at", 53212);
+
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            outputStream.writeBytes(mnr + "\n");
+
+        }catch (Exception e){
+            answer = "ERROR: crashed";
+        }
+    }
+}
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
