444a1f106e4e3c87e7477ae3423a3da0897533a6
SabrinaMuh <sabrina.muhrer@aon.at>
2022-03-06 12:26:49 +0100

    Update MainActivity.java
    create thread and build thread in app

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.se2_einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 class MatrikelnrThread extends Thread{
@@ -23,6 +29,10 @@ class MatrikelnrThread extends Thread{
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
             outputStream.writeBytes(mnr + "\n");
 
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            answer = inputReader.readLine();
+
+            socket.close();
         }catch (Exception e){
             answer = "ERROR: crashed";
         }
@@ -35,5 +45,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSend = (Button) findViewById(R.id.buttonSend);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText mnrText = (EditText) findViewById(R.id.editTextMatrikelNR);
+                TextView answer = (TextView) findViewById(R.id.textViewAnswer);
+                String mnr = mnrText.getText().toString();
+
+                MatrikelnrThread thread = new MatrikelnrThread(mnr);
+
+                thread.start();
+
+                try{
+                    thread.join();
+                }catch (InterruptedException ie){
+
+                }
+                answer.setText(thread.answer);
+            }
+        });
     }
 }
